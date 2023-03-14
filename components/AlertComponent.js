a094a5535ad14b550690b3b8719a3b03d0c56b34
hafidnurkholis <itsmaehere@gmail.com>
2023-03-14 16:58:32 +0700

    feat: Alert component

--- components/AlertComponent.js
+++ components/AlertComponent.js
@@ -14,7 +14,7 @@ const AlertComponent = ({
       color={color}
       icon={icon}
       show={show}
-      className={`z-50 ${position}`}
+      className={`z-50 fixed left-1/2 -translate-x-1/2 w-[97.5%] top-[2.5%]`}
       dismissible={{
         onClose: () => setShow(false),
         action: (
@@ -24,7 +24,15 @@ const AlertComponent = ({
         ),
       }}
     >
-      {message}
+      {typeof message === "string" && message}
+      {typeof message === "object" &&
+        !message.message &&
+        Object.values(message).map((element, index) => {
+          return <div>{element[0]}</div>;
+        })}
+      {typeof message === "object" && message.message && (
+        <div>{message.message}</div>
+      )}
     </Alert>
   );
 };
